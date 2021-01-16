1eacb698090ea1752a90be2f48fc4223d0b957e3
Thiago Bechara <thiagocbechara@gmail.com>
2021-01-16 19:53:26 -0300

    create api

---
+++ Games/Games.Infra/Db/DesignTimeDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Games.Infra.Db
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile(Directory.GetCurrentDirectory() + "/../Games.API/appsettings.json").Build();
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            var connectionString = configuration.GetConnectionString("DatabaseConnection");
+            builder.UseSqlServer(connectionString);
+            return new ApplicationDbContext(builder.Options);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # games-api
+
+## Configuration
+
+Execute DB Migration
+**.Net CLI**: ```dotnet ef migrations add Initial```
+**NuGet Package Manager Console**: ```Add-Migration Initial```
